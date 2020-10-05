68c30e58a5ac0d36aae4baf18c5bea2366ebb383
xuxw <xuxinwei@zhengheyinshi.com>
2020-10-05 20:22:22 +0800

    nacos-config init.

--- src/main/java/com/dunshan/biz/BaseApplication.java
+++ src/main/java/com/dunshan/biz/BaseApplication.java
@@ -3,6 +3,7 @@ package com.dunshan.biz;
 import com.dunshan.biz.client.PbClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
